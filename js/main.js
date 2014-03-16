bbc945060966d0cdb30bc64fa099cb932ee48e80
covingkj <covingkj@gmail.com>
2014-03-16 09:00:20 -0700

    Improve CSS, more code cleanup. Add sky as background, taken from dogeweather. Add some images of cats.

--- js/main.js
+++ js/main.js
@@ -5,22 +5,29 @@ function getWeather(link) {
         var id = data.weather[0].id;
         var icon = data.weather[0].icon;
 
-        $('#weather-id').text(id);
-        $('#weather-icon').text(icon);
-
 		var img = document.createElement("img");
 		img.setAttribute("src", 'img/kitteh/' + icon + '.png');
-        document.getElementById("kitteh-image").appendChild(img);
-
+		document.getElementById("kitteh-image").appendChild(img);
+        
+		var skaiUrl = 'img/skai/' + icon + '.png';
+        document.getElementById("main").style.backgroundImage = "url("+skaiUrl+")";
+        
+        $('#weather-id').text(id);
+        $('#weather-icon').text(icon);
+        
         var tempCelcius = data.main.temp - 273.15;
         var tempFahrenheit = tempCelcius * 9 / 5 + 32;
+        tempCelcius = Math.round(tempCelcius) + "°C";
+        tempFahrenheit = Math.round(tempFahrenheit) + "°F";
+        
         var description = data.weather[0].description;
+        var location    = data.name;
 
-        $('#weather-desc').text(description);
-        $('#locationName').text(data.name);
-
-        $('#degreesCelsius').text(Math.round(tempCelcius) + "°C");
-        $('#degreesFahrenheit').text(Math.round(tempFahrenheit) + "°F");
+        $('#weather-desc').text(tempCelcius + " " +
+        						tempFahrenheit + ", " +
+        						description + ", " +
+        						"in " + location + "?");
+        $('#weather-reaction').text("I can has");
 
         $(".ourinfo").show();
     });
